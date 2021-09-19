5fcfaa72455fc0a13d45d3b880aeef555f963f5d
Salvador Chinesta Llobregat <salvachll@gmail.com>
2021-09-19 18:13:53 +0200

    configuración firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBphq2PbxcI2izdW7I-7oYoKtUSGNOtBls",
+    authDomain: "powerbuypin.firebaseapp.com",
+    projectId: "powerbuypin",
+    storageBucket: "powerbuypin.appspot.com",
+    messagingSenderId: "271306468951",
+    appId: "1:271306468951:web:e3da914228559a0386fd12",
+    measurementId: "G-6T0L1NLVS1"
+  }
 };
 
 /*
