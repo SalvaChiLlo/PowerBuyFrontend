668d86028c8f03df42a624ef14570306ea29ba73
Salva <salvachll@gmail.com>
2021-10-12 18:56:06 +0200

    Nuevo search bar y selectode categorias

---
+++ src/app/main-page/categories-selector/categories-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-selector',
+  templateUrl: './categories-selector.component.html',
+  styleUrls: ['./categories-selector.component.css']
+})
+export class CategoriesSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
