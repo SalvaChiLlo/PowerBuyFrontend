f332eb667662711e1a43d70b08ab8cf1a1bb026c
Salva <salvachll@gmail.com>
2021-10-12 19:04:12 +0200

    preparación lista productos

--- src/app/main-page/product-list/product-list.component.ts
+++ src/app/main-page/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Product } from './../../models/producto.model';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
+  @Input() products: Product[] = [];
   constructor() { }
 
   ngOnInit(): void {
