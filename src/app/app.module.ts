4db7a00fcb13ed45f1c7cb5dee2f7f02db62f7ec
Salva <salvachll@gmail.com>
2021-10-12 16:59:25 +0200

    nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,12 @@ import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { ProductCardComponent } from './main-page/product-card/product-card.component';
+import { ProductListComponent } from './main-page/product-list/product-list.component';
+import { CategoriesSelectorComponent } from './main-page/categories-selector/categories-selector.component';
+import { SearchBarComponent } from './main-page/search-bar/search-bar.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component'
 
 @NgModule({
   declarations: [
@@ -17,7 +22,12 @@ import { HttpClientModule } from '@angular/common/http'
     BodyComponent,
     FooterComponent,
     MainPageComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    ProductCardComponent,
+    ProductListComponent,
+    CategoriesSelectorComponent,
+    SearchBarComponent,
+    ProgressBarComponent
   ],
   imports: [
     BrowserModule,
