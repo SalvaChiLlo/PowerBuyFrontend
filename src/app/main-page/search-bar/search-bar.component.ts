18f9cbdb53c8b251dcbd2423419fcaaa96c738de
Davido747 <mondavidmat@outlook.es>
2021-10-19 13:17:32 +0200

    Sacar string de la barra de buscador

--- src/app/main-page/search-bar/search-bar.component.ts
+++ src/app/main-page/search-bar/search-bar.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable, Subject } from 'rxjs';
+
+import { Product } from 'src/app/models/producto.model';
+
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
+  
+  private searchTerms = new Subject<string>();
 
   constructor() { }
 
+  search(term: string): void {
+    console.log(term);
+    this.searchTerms.next(term);
+  }
+
   ngOnInit(): void {
+    
   }
 
 }
