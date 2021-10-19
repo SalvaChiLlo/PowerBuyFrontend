b0165c04a43553afaf56f862582e2ebe5d671651
Sauuudi <iliasbox2@gmail.com>
2021-10-19 14:00:22 +0200

    ordenar rama creacion

--- src/app/main-page/sort-selector/sort-selector.component.ts
+++ src/app/main-page/sort-selector/sort-selector.component.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sort-selector.component.css']
 })
 export class SortSelectorComponent implements OnInit {
+  selectedOption: string;
+  printedOption: string;
 
+  options = [
+    {name: "relevancia", value : 1},
+    {name: "precioAscendente", value : 2},
+    {name: "precioDescendente", value : 3},
+    {name: "cantidadLoteAscendente", value : 4},
+    {name: "cantidadLoteDescendente", value : 5},
+    {name: "alfabeticamenteAscendente", value : 6},
+    {name: "alfabeticamenteDescendente", value : 7}
+  ]
+
+  cambio(data : any){
+    console.log(data.target.value)
+  }
+
+  
   constructor() { }
 
   ngOnInit(): void {
