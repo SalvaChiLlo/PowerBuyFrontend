e7363384857810a565d61d6a07fb035a4a216625
Salva <salvachll@gmail.com>
2021-10-20 15:17:41 +0200

    mejora en la busqueda

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -33,31 +33,31 @@ export class MainPageComponent implements OnInit {
       if (this.busqueda === '') {
         return true;
       } else if (this.busqueda !== '') {
-        return JSON.stringify(product).toLowerCase().includes(this.busqueda)
+        return JSON.stringify(product).toLowerCase().includes(this.busqueda.toLowerCase())
       } else {
         return false;
       }
 
     });
   }
-  getOrden(value: number){
+  getOrden(value: number) {
     this.sortType = value;
 
     //con el switch no se pk no funciona
     /*switch(value){
         default:
           //por defecto, como se programe en la busqueda
-          break;  
+          break;
 
         case 2: //precio ascendente
           this.productsToRender = this.products.sort((prd1, prd2) => prd1.precio - prd2.precio )
           break;
-          
+
         case 3: //precio descendente
           this.productsToRender = this.products.sort((prd1, prd2) => prd2.precio - prd1.precio )
           break;
 
-        case 4: //alfabeticamente ascendente 
+        case 4: //alfabeticamente ascendente
           this.productsToRender = this.products.sort(function (prd1, prd2 ){
             if(prd1.nombre.toLocaleLowerCase() > prd2.nombre.toLocaleLowerCase()){
                 return 1;
@@ -69,7 +69,7 @@ export class MainPageComponent implements OnInit {
           });
 
           break;
-              
+
         case 5:  //alfabeticamente descendente
           this.productsToRender = this.products.sort(function (prd1, prd2 ){
           if(prd1.nombre.toLocaleLowerCase() < prd2.nombre.toLocaleLowerCase()){
@@ -82,54 +82,54 @@ export class MainPageComponent implements OnInit {
 
           });
           break;
-                
+
         case 6:  //cantidad lote ascendente
           this.productsToRender = this.products.sort((prd1, prd2) => (prd2.cantidadInicial/prd2.cantidadDisponible) - (prd1.cantidadInicial/prd1.cantidadDisponible) )
           break;
-                  
+
         case 7:  //cantidad lote descendente
           this.productsToRender = this.products.sort((prd1, prd2) => (prd1.cantidadInicial/prd1.cantidadDisponible) - (prd2.cantidadInicial/prd2.cantidadDisponible) )
           break;
     }*/
 
-    if(value == 2){
-      this.productsToRender = this.products.sort((prd1, prd2) => prd1.precio - prd2.precio )
+    if (value == 2) {
+      this.productsToRender = this.products.sort((prd1, prd2) => prd1.precio - prd2.precio)
     }
-    else if(value == 3){
-      this.productsToRender = this.products.sort((prd1, prd2) => prd2.precio - prd1.precio )
+    else if (value == 3) {
+      this.productsToRender = this.products.sort((prd1, prd2) => prd2.precio - prd1.precio)
     }
-    else if(value == 4){
-      this.productsToRender = this.products.sort(function (prd1, prd2 ){
-        if(prd1.nombre.toLocaleLowerCase() > prd2.nombre.toLocaleLowerCase()){
-            return 1;
+    else if (value == 4) {
+      this.productsToRender = this.products.sort(function (prd1, prd2) {
+        if (prd1.nombre.toLocaleLowerCase() > prd2.nombre.toLocaleLowerCase()) {
+          return 1;
         }
-        else if(prd1.nombre.toLocaleLowerCase() < prd2.nombre.toLocaleLowerCase()){
-            return -1;
+        else if (prd1.nombre.toLocaleLowerCase() < prd2.nombre.toLocaleLowerCase()) {
+          return -1;
         }
         return 0;
       });
     }
-    else if(value == 5){
-      this.productsToRender = this.products.sort(function (prd1, prd2 ){
-        if(prd1.nombre.toLocaleLowerCase() < prd2.nombre.toLocaleLowerCase()){
-            return 1;
+    else if (value == 5) {
+      this.productsToRender = this.products.sort(function (prd1, prd2) {
+        if (prd1.nombre.toLocaleLowerCase() < prd2.nombre.toLocaleLowerCase()) {
+          return 1;
         }
-        else if(prd1.nombre.toLocaleLowerCase() > prd2.nombre.toLocaleLowerCase()){
-            return -1;
-         }
+        else if (prd1.nombre.toLocaleLowerCase() > prd2.nombre.toLocaleLowerCase()) {
+          return -1;
+        }
         return 0;
-        });
+      });
     }
-    else if(value == 6){
-      this.productsToRender = this.products.sort((prd1, prd2) => (prd2.cantidadInicial/prd2.cantidadDisponible) - (prd1.cantidadInicial/prd1.cantidadDisponible) )
+    else if (value == 6) {
+      this.productsToRender = this.products.sort((prd1, prd2) => (prd2.cantidadInicial / prd2.cantidadDisponible) - (prd1.cantidadInicial / prd1.cantidadDisponible))
     }
-    else if(value == 7){
-      
-      this.productsToRender = this.products.sort((prd1, prd2) => (prd1.cantidadInicial/prd1.cantidadDisponible) - (prd2.cantidadInicial/prd2.cantidadDisponible) )
+    else if (value == 7) {
+
+      this.productsToRender = this.products.sort((prd1, prd2) => (prd1.cantidadInicial / prd1.cantidadDisponible) - (prd2.cantidadInicial / prd2.cantidadDisponible))
     }
 
     this.getBusqueda(this.busqueda)
-    
+
   }
 }
 
