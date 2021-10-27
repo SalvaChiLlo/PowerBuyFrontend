bd3b322d71d5f41323b44f348ff470ab1875aec9
Salva <salvachll@gmail.com>
2021-10-27 08:15:15 +0200

    Solucionar orden

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -18,7 +18,7 @@ export class MainPageComponent implements OnInit {
   categoria: string = '0';
   productsCategorias: Product[];
   productsBusqueda: Product[];
-  sortType : any;
+  sortType: any;
   constructor(private productService: ProductsService, private categoriesService: CategoriasService) { }
 
   ngOnInit(): void {
@@ -63,52 +63,51 @@ export class MainPageComponent implements OnInit {
   getOrden(value: number) {
     this.sortType = value;
 
-    //con el switch no se pk no funciona
-    /*switch(value){
-        default:
-          //por defecto, como se programe en la busqueda
-          break;
-
-        case 2: //precio ascendente
-          this.productsToRender = this.products.sort((prd1, prd2) => prd1.precio - prd2.precio )
-          break;
-
-        case 3: //precio descendente
-          this.productsToRender = this.products.sort((prd1, prd2) => prd2.precio - prd1.precio )
-          break;
-
-        case 4: //alfabeticamente ascendente
-          this.productsToRender = this.products.sort(function (prd1, prd2 ){
-            if(prd1.nombre.toLocaleLowerCase() > prd2.nombre.toLocaleLowerCase()){
-                return 1;
-            }
-            else if(prd1.nombre.toLocaleLowerCase() < prd2.nombre.toLocaleLowerCase()){
-                return -1;
-            }
-            return 0;
-          });
-
-          break;
-
-        case 5:  //alfabeticamente descendente
-          this.productsToRender = this.products.sort(function (prd1, prd2 ){
-          if(prd1.nombre.toLocaleLowerCase() < prd2.nombre.toLocaleLowerCase()){
-              return 1;
-          }
-          else if(prd1.nombre.toLocaleLowerCase() > prd2.nombre.toLocaleLowerCase()){
-              return -1;
-            }
-          return 0;
-
-          });
-          break; */
+    if (value == 2) {
+      this.productsToRender = this.products.sort((prd1, prd2) => prd1.precio - prd2.precio)
+    }
+    else if (value == 3) {
+      this.productsToRender = this.products.sort((prd1, prd2) => prd2.precio - prd1.precio)
+    }
+    else if (value == 4) {
+      this.productsToRender = this.products.sort(function (prd1, prd2) {
+        if (prd1.nombre.toLocaleLowerCase() > prd2.nombre.toLocaleLowerCase()) {
+          return 1;
         }
+        else if (prd1.nombre.toLocaleLowerCase() < prd2.nombre.toLocaleLowerCase()) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+    else if (value == 5) {
+      this.productsToRender = this.products.sort(function (prd1, prd2) {
+        if (prd1.nombre.toLocaleLowerCase() < prd2.nombre.toLocaleLowerCase()) {
+          return 1;
+        }
+        else if (prd1.nombre.toLocaleLowerCase() > prd2.nombre.toLocaleLowerCase()) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+    else if (value == 6) {
+      this.productsToRender = this.products.sort((prd1, prd2) => (prd2.cantidadInicial / prd2.cantidadDisponible) - (prd1.cantidadInicial / prd1.cantidadDisponible))
+    }
+    else if (value == 7) {
+
+      this.productsToRender = this.products.sort((prd1, prd2) => (prd1.cantidadInicial / prd1.cantidadDisponible) - (prd2.cantidadInicial / prd2.cantidadDisponible))
+    }
+
+    this.mergeProductos()
+  }
+
   mergeProductos() {
-    if(this.busqueda !== '' && this.categoria !== '' && this.categoria!== '0') {
+    if (this.busqueda !== '' && this.categoria !== '' && this.categoria !== '0') {
       this.productsToRender = this.productsBusqueda.filter(pb => {
         return this.productsCategorias.includes(pb)
       })
-    } else if(this.busqueda !== '') {
+    } else if (this.busqueda !== '') {
       this.productsToRender = this.productsBusqueda
     } else {
       this.productsToRender = this.productsCategorias
