a779aa7d2989d2429a09506ae2274a1d6c5700ab
SalvaChLl <salvachll@gmail.com>
2021-11-10 12:03:03 +0100

    prueba

--- cypress/integration/prueba.spec.ts
+++ cypress/integration/prueba.spec.ts
@@ -1,7 +1,7 @@
 describe('Prueba', () => {
   it('Prueba', () => {
-    cy.visit('http://localhost:2400');
-    cy.wait(5000)
+    cy.visit('http://localhost:4200');
+    cy.wait(15000)
     cy.get('body').should('exist')
   })
 })
