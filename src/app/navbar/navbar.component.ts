0097a35403a723bca297bb1a16712d8dbf5096e4
SalvaChLl <SalvaChLl@gmail.com>
2021-12-02 09:12:06 +0100

    fix categorias

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -94,6 +94,7 @@ export class NavbarComponent implements OnInit {
 
         if (ev.url === '/' && ev.urlAfterRedirects === '/home') {
           this.showBack = false;
+          this.showCategorias = true
         }
       }
     })
